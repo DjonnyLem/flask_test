3cdb9c07b8d90fbbfc9e8102f44f1bab98ebeb3f
DjonnyLem <Djonnylem@gmail.com>
2021-02-26 21:57:27 +0400

    26.02.2021 21:39 (#1)

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
-from flask import request, redirect
+
 
 
 @app.template_filter("clean_date")
@@ -145,3 +145,24 @@ def multiple(foo, bar, baz):
 
 
     return f"foo is {foo}, bar is {bar}, baz is {baz}"
+
+
+@app.route("/json", method=["POST"])
+def json_example():
+        
+        # Validate the request body contains JSON
+        if request.get_json:
+
+                # Parse the JSON into a Python dictionary
+                req = request.get_json()
+
+                # Print the dictionary
+                print(req)
+
+                # Return a string along with an HTTP status code
+                return "JSON received!", 200
+
+        else:
+
+                # The request body wasn't JSON so return a 400 HTTP status code
+                return "Request was not JSON", 400     return 'Thanks!', 200
